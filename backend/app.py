804d8c6c579fe458cba611fcbb387f93b79409a0
Thanakorn <philfilm2004@gmail.com>
2025-09-29 20:33:26 +0700

    origin

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 import fastapi as _fastapi
 import uvicorn as _uvicorn
-import os as _os
 import logging as _logging
 from fastapi.middleware.cors import CORSMiddleware as _CORSMiddleware
+from sklearn.linear_model import LogisticRegression as _LogisticRegression
+import os
+
